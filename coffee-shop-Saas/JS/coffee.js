6bbc79d4e86fa84fefb4df7f888c8dfce1ecfa47
filipoint <philipihoricaniye@gmail.com>
2026-03-04 22:01:49 +0200

    Chapitre 5 - Ajout panier dynamique Coffee Shop

--- coffee-shop-Saas/JS/coffee.js
+++ coffee-shop-Saas/JS/coffee.js
@@ -41,10 +41,26 @@ for(let i=0; i<products.length; i++){
 
 /* ajouter un événement au bouton pour afficher une alerte avec le produit sélectionné*/
 button.addEventListener("click", function(){
-    if(selectedProduct){
-        alert("Vous avez commandé : "+selectedProduct.textContent);
-    }else{
+    if(!selectedProduct){
         alert("Veuillez sélectionner un produit avant de commander.");
+    }else{
+        let item = document.createElement("p");
+        item.textContent = selectedProduct.textContent; //afficher le nom du produit sélectionné dans le panier 
+        
+        /* ajouter une classe de style*/
+        item.classList.add("cart-item");
+
+        let cart = document.querySelector("#cart");
+        
+        /* ajouter l'élément au panier*/
+        cart.appendChild(item);
+        
+        /* réinitialiser la sélection*/
+        selectedProduct.classList.remove("selected");
+        selectedProduct=null;
+        title.textContent="Veuillez sélectionner un produit";
     }
 
 });
+
+
